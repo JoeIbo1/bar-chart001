e227ad17d390387f7e77eecd22c5878eaf391e79
JoeIbo1 <124044238+JoeIbo1@users.noreply.github.com>
2023-04-12 11:44:08 +0200

    display data is done

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
 
 window.addEventListener("load", start);
 
-const data = [9, 31, 8, 22, 23, 17, 25, 26, 23, 7, 27, 9, 13, 14, 11, 12, 5, 28, 5, 1, 25, 0, 2, 28, 31, 10, 19, 31, 6, 8, 19, 22, 14, 0, 8, 0, 26, 4, 3, 3, 17]
+const data = [16, 31, 8, 22, 23, 17, 25, 26, 23, 7, 27, 9, 13, 14, 11, 12, 5, 28, 5, 1, 25, 0, 2, 28, 31, 10, 19, 31, 6, 8, 19, 22, 14, 0, 8, 0, 26, 4, 3, 3, 17]
 
 
 function start() {
     console.log("hi java")
 
+    displayData();
+}
+
+function displayData() {
     const bars = document.querySelectorAll(".bar");
 
-    const firstBar = bars[0];
+    for(let i = 0; i < 40; i++){
+        const currentBar = bars[i];
+        const h = data[i] / 32*100;
 
-    const h = data[0] / 32*100;
-
-    firstBar.style.height = `${h}px`;
+        currentBar.style.height = `${h}px`;
+    }
 }
