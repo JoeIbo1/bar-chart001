bd97d364d1953246f1a7536fc284481ff2ff6c6d
JoeIbo1 <124044238+JoeIbo1@users.noreply.github.com>
2023-04-12 22:54:10 +0200

    last updates finished

--- script.js
+++ script.js
@@ -3,16 +3,17 @@
 window.addEventListener("load", start);
 
 const data = [16, 31, 8, 22, 23, 17, 25, 26, 23, 7, 27, 9, 13, 14, 11, 12, 5, 28, 5, 1, 25, 0, 2, 28, 31, 10, 19, 31, 6, 8, 19, 22, 14, 0, 8, 0, 26, 4, 3, 3, 17]
-
+const queue = initArry();
 
 function start() {
-    console.log("hi java")
-
     displayData();
+    setInterval(displayData, 1000)
 }
 
 function displayData() {
     const bars = document.querySelectorAll(".bar");
+
+    updateQueue();
 
     for(let i = 0; i < 40; i++){
         const currentBar = bars[i];
@@ -20,4 +21,21 @@ function displayData() {
 
         currentBar.style.height = `${h}px`;
     }
+}
+
+function getNumberOfCustomers(){
+    return Math.floor(Math.random() * 32);
+}
+
+function initArry(){
+    for (let i = 0; i < 40; i++) {
+        data[i] = getNumberOfCustomers();
+      }
+      return data;
+}
+
+function updateQueue() {
+    const queueSize = getNumberOfCustomers();
+    queue.push(queueSize);
+    queue.shift();
 }
