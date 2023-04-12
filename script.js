2165dbf09fece21603ad34bedb1b4df92f0cda22
JoeIbo1 <124044238+JoeIbo1@users.noreply.github.com>
2023-04-12 11:34:27 +0200

    tilføjer data til et array

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 window.addEventListener("load", start);
 
+const data = [9, 31, 8, 22, 23, 17, 25, 26, 23, 7, 27, 9, 13, 14, 11, 12, 5, 28, 5, 1, 25, 0, 2, 28, 31, 10, 19, 31, 6, 8, 19, 22, 14, 0, 8, 0, 26, 4, 3, 3, 17]
+
+
 function start() {
     console.log("hi java")
 
